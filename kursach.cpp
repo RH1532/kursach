24ddb908daff287fcf2a15f90bf81a767b83f358
RH1532 <65445926+RH1532@users.noreply.github.com>
2020-05-30 21:11:01 +0300

    Add files via upload

---
+++ kursach.cpp
@@ -0,0 +1,21 @@
+﻿#include "fstream"
+#include "iostream"
+#include "Network.h"
+
+using namespace std;
+
+int main()
+{
+	try {
+		int source, sink;
+		ifstream input("C:\\users\\danil\\source\\repos\\kursach\\kursach\\input.txt");
+		Network* transnet = new Network(input);
+		int MaxFlow = transnet->max_flow();
+		cout << endl << "Max Flow is " << MaxFlow << endl;
+		delete transnet;
+	}
+
+	catch (exception& ex) {
+		std::cout << ex.what();
+	}
+}
